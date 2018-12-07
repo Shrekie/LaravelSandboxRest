2d5e7c8eb7cc007b23ea719c6f7d4284a86df18f
root <thomaslindauer@mail.com>
2018-12-08 00:59:48 +0100

    passport, vue mix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,7 +92,18 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+
+
+                <div id="app">
+                    <example-component></example-component>
+                    <passport-clients></passport-clients>
+                    <passport-authorized-clients></passport-authorized-clients>
+                    <passport-personal-access-tokens></passport-personal-access-tokens>
+                </div>
+            
             </div>
         </div>
+
+
     </body>
 </html>
