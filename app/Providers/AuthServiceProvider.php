eb44245a771a1022fd50656e061b9931ef72126e
Thomas Lindauer <thomaslindauer@mail.com>
2018-12-10 21:56:04 +0100

    Disable OAuth client generation api, CreateFreshApiToken

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,8 +26,13 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Passport::routes();
+        Passport::routes(
+            function ($router) {
+                $router->forAuthorization();
+                $router->forAccessTokens();
+                $router->forTransientTokens();
+            }
+        );
         
-        //
     }
 }
